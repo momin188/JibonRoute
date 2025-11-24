574932d144b2d2a2bfa793f8ab4b6f18051e1058
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-11-24 18:36:38 +0600

    Refactor capacitor.config.ts for consistency and clarity

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.jibonroute.app',
-  appName: 'Jibon Route',
-  webDir: 'dist'
+  appId: "com.jibonroute.app",
+  appName: "Jibon Route",
+  webDir: "dist",
+  server: {
+    url: "http://100.84.154.38:8080",
+    cleartext: true,
+  },
 };
 
 export default config;
