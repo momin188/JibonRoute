866643d3b5043f33feecaa25c845e433ea72580e
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-11-24 18:23:35 +0600

    Add capacitor configuration and dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jibonroute.app',
+  appName: 'Jibon Route',
+  webDir: 'dist'
+};
+
+export default config;
